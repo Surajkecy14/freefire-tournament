f723aa34e83529e72eac98b5b3de3448f4d70acb
Surajkecy14 <147683761+Surajkecy14@users.noreply.github.com>
2025-06-19 20:56:49 +0545

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const cookieParser = require("cookie-parser");
 
 const app = express();
-const port =  5000;
+const port =  process.env.PORT || 5000;
 
 // Middleware
 app.use(cookieParser());
